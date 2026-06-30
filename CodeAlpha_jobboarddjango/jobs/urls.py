a1220b706bcfd3016ca04c1eb32f9d69c7442c35
sai jetendra <saijithendra807@gmail.com>
2026-06-30 15:44:39 +0530

    Delete CodeAlpha_jobboarddjango directory

--- CodeAlpha_jobboarddjango/jobs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns=[
-    path('employers/',EmployerListCreate.as_view()),
-    path('candidates/',CandidateListCreate.as_view()),
-    path('jobs/',JobListCreate.as_view()),
-    path('applications/',ApplicationListCreate.as_view()),
-]
